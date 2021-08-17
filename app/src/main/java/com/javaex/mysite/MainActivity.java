e7f066ddfaac7f751220436e37587d269c8b034c
tykaind <cult40@naver.com>
2021-08-17 10:17:59 +0900

    1st commit

---
+++ app/src/main/java/com/javaex/mysite/MainActivity.java
@@ -0,0 +1,19 @@
+package com.javaex.mysite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //필드
+    //생성자
+    //gs
+    //일반
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
